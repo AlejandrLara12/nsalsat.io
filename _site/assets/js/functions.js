2c894f9d96df4566807bc09c561f9c633a957f0d
AlejandrLara12 <alejandro.elara@live.com>
2016-12-24 16:07:11 -0600

    All started up files added

---
+++ _site/assets/js/functions.js
@@ -0,0 +1,26 @@
+var main = function() {
+
+
+// When a linked is clicked it auto scroll up to that ID
+// $('.day').click(function(event){
+//     // a[href*=".html#"] contains
+//     // a[href$=".html#"] ends
+//     // a[href^=".html#"] starts
+//     var duration = 300;
+//     var url = $(this).attr('href');
+//     var target = url;
+//     console.log(target)
+//
+//     if( target.length ){
+//         event.preventDefault();
+//         $('html, body').animate({
+//           scrollTop: target.offset().top
+//         },duration);
+//     }
+//
+// });
+
+
+}// ends main
+
+$(document).ready(main);
